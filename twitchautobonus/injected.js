ddea57a9e7182bef73a73978bc0f685ccbda65a4
jensk-dev <jenskresse@live.nl>
2020-06-13 18:52:22 +0200

    v1.2

--- twitchautobonus/injected.js
+++ twitchautobonus/injected.js
@@ -1,4 +1,7 @@
-setInterval(function() {
-	var el = document.querySelector("div > div.tw-block.tw-flex-grow-0.tw-flex-shrink-0.tw-full-height.tw-relative > div > div > section > div > div.chat-input.tw-block.tw-pd-b-1.tw-pd-x-1 > div:nth-child(2) > div.chat-input__buttons-container.tw-flex.tw-justify-content-between.tw-mg-t-1 > div:nth-child(1) > div > div > div > div.tw-full-height.tw-relative.tw-z-above > div > div > div > button");
+const selector = "div.tw-full-height.tw-relative.tw-z-above > div > div > div > button";
+const handler = function() {
+	const el = document.querySelector(selector);
 	if (el) el.click();
-}, 2000);
+}
+
+setInterval(handler, 5000);
