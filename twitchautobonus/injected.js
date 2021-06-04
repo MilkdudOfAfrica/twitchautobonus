ace1cc82b855d1ab50bba777ca4cce1f7c1fc9ea
Matthew Warren <mw@matthewwarren.dev>
2021-06-03 23:27:26 -0500

    Update injected.js
    Updated with what works now

--- twitchautobonus/injected.js
+++ twitchautobonus/injected.js
@@ -1,4 +1,4 @@
-const selector = "div.tw-full-height.tw-relative.tw-z-above > div > div > div > button";
+const selector = "div.community-points-summary > div > div > div > div > button";
 const handler = function() {
 	const el = document.querySelector(selector);
 	if (el) el.click();
